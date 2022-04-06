8d9390d784e712380fb22a84857cd158fba8cd9c
HanLuong13 <hluong13@gmail.com>
2022-04-06 11:56:44 -0500

    added more logic

--- exercise_word_guessing_game.py
+++ exercise_word_guessing_game.py
@@ -1,4 +1,11 @@
 word = 'random'
+
+total_guess = 2
+guess_count = 0 
+
+guess_list = []
+guess_list_wrong = []
+
 def display_word():
     word_display = ''
     for l in word:
@@ -7,27 +14,32 @@ def display_word():
         else:
             word_display += '_'
     print(word_display)
-    
-display_word()
-total_guess = 7
-guess_count = 0 
 
-guess_list = []
-
+# print('Word: ', display_word())
 
 while guess_count < total_guess:
     guess = input('Enter your guess: ').lower()
     # validate_guess(guess)
     if guess.isalpha():
         if guess == word:
-            print('You won. Word is ' + word)
+            print('You won! Word is ' + word + '!')
+            guess_count += 1
             break
         elif guess in word: 
             guess_list.append(guess)
             print(guess)
             display_word()
+        else:
+            if guess in guess_list_wrong:
+                print('You have already tried this. Do better!')
+            else:
+                print('Letter not in word')
+                guess_count += 1
+                guess_list_wrong.append(guess)
     else:
         print('Try again. Enter a word or letter this time')
+
+print('You\'ve tried', guess_count, 'times. You lost. Game over!')
 
 # def validate_guess(guess):
 #     if guess.isalpha():

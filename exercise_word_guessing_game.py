0b63e4867c4f19cd9097ae37473731485817c7ad
HanLuong13 <hluong13@gmail.com>
2022-04-06 11:42:02 -0500

    added word display

--- exercise_word_guessing_game.py
+++ exercise_word_guessing_game.py
@@ -1,16 +1,36 @@
-import string
-
-
 word = 'random'
-
+def display_word():
+    word_display = ''
+    for l in word:
+        if l in guess_list:
+            word_display += l
+        else:
+            word_display += '_'
+    print(word_display)
+    
+display_word()
 total_guess = 7
 guess_count = 0 
 
+guess_list = []
+
+
 while guess_count < total_guess:
-    guess = input('Enter your guess: ')
-    print(guess)
+    guess = input('Enter your guess: ').lower()
+    # validate_guess(guess)
     if guess.isalpha():
-        print('valid')
+        if guess == word:
+            print('You won. Word is ' + word)
+            break
+        elif guess in word: 
+            guess_list.append(guess)
+            print(guess)
+            display_word()
     else:
         print('Try again. Enter a word or letter this time')
-        
+
+# def validate_guess(guess):
+#     if guess.isalpha():
+#         print('valid')
+#     else:
+#         print('Try again. Enter a word or letter this time')

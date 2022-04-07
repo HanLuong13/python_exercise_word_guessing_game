7fd46b701c79d480b336aa592028f02a48528136
HanLuong13 <hluong13@gmail.com>
2022-04-07 08:09:40 -0500

    partial part 5, added readme, and txt file

--- exercise_word_guessing_game.py
+++ exercise_word_guessing_game.py
@@ -1,31 +1,26 @@
 import random
+
 total_losses = 0
 total_wins = 0
 num_plays = 1
+all_picked_words = []
 
-# def loss_count():
-#     global total_losses
-#     total_losses += 1
-#     return total_losses
-
+#displaying word
 def display_word(input):
     global word_display
     word_display = ''
     for l in input:
-        if l in already_guessed_right:
+        if l in already_guessed_letter_right:
             word_display += l
         else:
             word_display += '_'
-    print('Word: ')
-    print(word_display)
+    print(f'Word: {word_display}')
     return word_display
 
+#picking random words, making sure not already picked
 def pick_word():
-    global all_picked_words 
-    global word
-    all_picked_words = []
-    file_path = (r'H:\words_alpha.csv')
-    file = open(file_path)
+    global all_picked_words, word 
+    file = open('words_alpha.txt')
     word_list = file.readlines()
     word = random.choice(word_list).strip()
     unique_word = False
@@ -37,63 +32,77 @@ def pick_word():
     all_picked_words.append(word)
     return word
 
+#main game function
 def play_game():
-    global total_losses, total_wins
-    global already_guessed_right
-    global guessed_right
-    global guess_count
-    already_guessed_right = []
-    already_guessed_wrong = {}
-    word_guess_list_wrong = set()
+    global total_losses, total_wins, already_guessed_letter_right, guessed_right, guesses_left, total_guess, word_display
+    guesses_left = 7
+    already_guessed_letter_right = []
+    already_guessed_letter_wrong = {}
+    already_guessed_word_wrong = set()
+    total_guess = 0
     word = pick_word()
-    display_word(word)
-    total_guess = 2
-    guess_count = 0
     guessed_right = False
-    while guess_count < total_guess and not guessed_right:
-        guess = input('Enter your guess: ').lower()
-        # validate_guess(guess)
+    while guesses_left > 0 and not guessed_right:
+        display_word(word)
+        # if word was revealed through letter choices
+        if '_' not in word_display:
+            guessed_right = True 
+            break
+        # word not revealed yet, continue with user input
+        else:
+            print(f'You have {guesses_left} guesses left.')
+            guess = input('Enter your guess: ').lower()
+        #validate word is alphabetical
         if guess.isalpha():
-            if len(guess) > 1: #if it's a word
+            # input is a word
+            if len(guess) > 1: 
+                # & input is THE word
                 if guess == word:
-                    guess_count += 1
+                    guesses_left -= 1
+                    total_guess += 1
                     guessed_right = True
                     break
-                elif guess in word_guess_list_wrong:
-                    print('You have already tried this word. Do better!')
-                elif guess != word: 
-                    print('Not the word. Try again.')  
-                    word_guess_list_wrong.add(guess) 
-                    guess_count += 1
-            elif len(guess) == 1:  #if it's a letter 
-                if guess in word and guess in already_guessed_right: 
-                    print('You have already tried this letter. Do better!')
-                elif guess in word and guess not in already_guessed_right:
-                    already_guessed_right.append(guess)
-                    print(guess) 
-                    display_word(word)
-                    if '_' not in word_display:   
-                        guessed_right = True 
-                        break
-                elif guess in already_guessed_wrong.values():
-                    print('You have already tried this. It wasn\'t in the word. Do better!')
+                # & input is a word that has already been guessed
+                elif guess in already_guessed_word_wrong:
+                    print(f'You\'ve already tried this word. Do better! ')
+                # & input is a word that hasn't been guessed but is wrong
+                elif guess != word:   
+                    already_guessed_word_wrong.add(guess) 
+                    guesses_left -= 1
+                    total_guess += 1
+                    print(f'Not the word. Try again.')
+            # input is a letter
+            elif len(guess) == 1:  
+                # & input is in word and already guessed
+                if guess in word and guess in already_guessed_letter_right: 
+                    print(f'You\'ve already tried this letter. Do better!')
+                # & input is in word but not already guessed
+                elif guess in word and guess not in already_guessed_letter_right:
+                    already_guessed_letter_right.append(guess)
+                    print('You got one.') 
+                # & input is NOT in word and is already guessed                        
+                elif guess in already_guessed_letter_wrong.values():
+                    print(f'You\'ve already tried this letter. It wasn\'t in the word. Do better!')
+                # & input is NOT in word and not previously guessed
                 else: 
-                    print('Letter not in word. Try again.')
-                    guess_count += 1
-                    already_guessed_wrong[guess_count] = guess                    
-        else: #not valid input
+                    guesses_left -= 1
+                    total_guess += 1
+                    already_guessed_letter_wrong[total_guess] = guess  
+                    print(f'Letter not in word. Try again.')                  
+        # input is not valid 
+        else: 
             print('Try again. Enter a word or letter this time')
+    # guessed right
     if guessed_right:
         print('You won! The word is {}!'.format(word))
         total_wins += 1
+    # out of guesses 
     else:
-        print('You\'ve tried {} times. You lost. The word is {}. Game over!'.format(guess_count, word))
+        print('You\'ve tried {} times. You lost. The word is {}. Game over!'.format(total_guess, word))
         total_losses += 1
     play_again()
 
-    # return num_plays
-    # return total_wins, total_losses
-
+# asks if player wants to play again
 def play_again():
     global num_plays
     play_again = False
@@ -104,34 +113,14 @@ def play_again():
             num_plays += 1
             play_game()
         elif replay == 'n':
-            print('That is fine. You have played {} times.'.format(num_plays))
+            print('That is fine. See you next time!')
             break
         else:
             print('Not valid input. Try again.')
 
-# def win_count():
-#     global total_wins
-#     total_wins = 0
-#     total_wins += 1
-#     return total_wins
-
-# def loss_count():
-#     global total_losses
-#     total_losses = 0 
-#     total_losses += 1
-#     return total_losses
-
-# print('Total wins: {}'.format(total_wins))
-
-
 
 play_game()
-print('Total losses: {}'.format(total_losses))
-print('Number of times played: ', num_plays)
+print(f'You\'ve played {num_plays} times. You\'ve won {total_wins} times. You\'ve lost {total_losses} times.')
+print('The words were', all_picked_words)
 
-# def validate_guess(guess):
-#     if guess.isalpha():
-#         print('valid')
-#     else:
-#         print('Try again. Enter a word or letter this time')
 
